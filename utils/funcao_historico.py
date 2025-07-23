6bd226a8af87500a3c73df70210eaafe79f70616
Yuri Fabio <yuri.fabio@escolar.ifrn.edu.br>
2025-07-23 19:39:24 -0300

    adcionando instruções em funcao_historico.py

--- utils/funcao_historico.py
+++ utils/funcao_historico.py
@@ -1,5 +1,5 @@
 # Essa função adciona os dados a um arquivo.txt 
-# Dados necessarios → 
+# Dados necessarios → Data, hora e lista do sorteio
 
 with open('historico.txt', 'a', encoding='utf-8') as arquivo:
         dados = arquivo.write('\n')
