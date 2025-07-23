ad814566f764fa2caafc183490eb70cd0e1c4ab9
Yuri Fabio <yuri.fabio@escolar.ifrn.edu.br>
2025-07-23 19:30:51 -0300

    adcionando funções de exibição e armazenamento

---
+++ utils/exibicao.py
@@ -0,0 +1,4 @@
+# Essa função exibe os dados na tela do usuário
+
+def exibir(numeros_sorteados, numeros_adcionados, quantidade_de_acertos, numeros_acertados):
+    pass
--- utils/funcao_historico.py
+++ utils/funcao_historico.py
@@ -1,6 +1,6 @@
-# Essa função adciona os dados a um arquivo.txt
+# Essa função adciona os dados a um arquivo.txt 
+# Dados necessarios → 
 
 with open('historico.txt', 'a', encoding='utf-8') as arquivo:
         dados = arquivo.write('\n')
         pass
-        
