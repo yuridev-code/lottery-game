115bda54997bf390a1bf24b013fa2cbb2661d1d9
Kaua Henrique <kaua.h@academico.ifrn.edu.br>
2025-07-28 20:39:17 -0300

    Atualizacoes com parte das implementacoes das funcoes

--- interface/pagina_sorteio.py
+++ interface/pagina_sorteio.py
@@ -4,13 +4,11 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from interface.pagina_historico import HistoricoJanela
-
-# Funções temporárias (mock)
-def sortear_numeros_loteria():
-    return {5, 12, 23, 30, 44, 60}
-
-def verificar_acertos(numeros_usuario, numeros_sorteados):
-    return numeros_usuario.intersection(numeros_sorteados)
+from utils.sorteio import gerar_sorteio
+from utils.comparacao import comparar_apostas
+from utils.registro import registro
+from utils.exibicao import exibir
+from utils.historico import historico
 
 def premiar_jogador(qtd_acertos):
     if qtd_acertos >= 6:
@@ -22,8 +20,6 @@ def premiar_jogador(qtd_acertos):
     else:
         print("Sem prêmio.")
 
-def registrar_operacao(numeros_usuario, numeros_sorteados):
-    print("Operação registrada.")
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
@@ -53,27 +49,20 @@ class SorteioApp(ctk.CTk):
         HistoricoJanela()
 
     def realizar_sorteio(self):
-        entrada_usuario = self.entrada.get()
-        try:
-            numeros_usuario = set(int(num.strip()) for num in entrada_usuario.split(",") if 1 <= int(num.strip()) <= 60)
-            if len(numeros_usuario) != 6:
-                self.resultado.configure(text="Você deve digitar exatamente 6 números válidos.")
-                return
-        except:
-            self.resultado.configure(text="Entrada inválida. Use apenas números entre 1 e 60.")
-            return
+        numeros_usuario = [
+            int(self.entrada1.get()),
+            int(self.entrada2.get()),
+            int(self.entrada3.get()),
+            int(self.entrada4.get()),
+            int(self.entrada5.get()),
+            int(self.entrada6.get())
+        ]
+        numeros_sorteados = gerar_sorteio()
+        acertos, qtd_acertos = comparar_apostas(numeros_usuario, numeros_sorteados)
 
-        numeros_sorteados = sortear_numeros_loteria()
-        acertos = verificar_acertos(numeros_usuario, numeros_sorteados)
-
-        registrar_operacao(numeros_usuario, numeros_sorteados)  # Salva em registro.txt
-
-        resultado_texto = f"Números sorteados: {sorted(numeros_sorteados)}\n" \
-                          f"Seus números: {sorted(numeros_usuario)}\n" \
-                          f"Acertos: {sorted(acertos)}\n"
-        self.resultado.configure(text=resultado_texto)
-
-        premiar_jogador(len(acertos))  # Exibe mensagem no terminal
+        registro(numeros_sorteados, numeros_usuario, qtd_acertos, acertos)
+        historico(numeros_sorteados, numeros_usuario, qtd_acertos, acertos)
+        exibir(numeros_sorteados, numeros_usuario, qtd_acertos, acertos)
 
 def abrir_pagina_sorteio(janela_anterior=None):
     if janela_anterior:
