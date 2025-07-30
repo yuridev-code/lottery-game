927d358deadddecff04235aa731cba57e31c5037
CARLOS MATEUS DE CARVALHO GONÇALVES <20241148060031@ifrn.local>
2025-07-30 15:02:37 -0300

    adicao da funcao gerar_id no historico

--- utils/historico.py
+++ utils/historico.py
@@ -1,6 +1,6 @@
-def historico(sorteio, aposta, acertos, numeros_acertados):
+def historico(sorteio, aposta, acertos, numeros_acertados, gerar_id):
     with open("dados/historico.txt", 'a', encoding="utf-8") as f:
         texto_acertados = "Nenhum" if acertos == 0 else ", ".join(map(str, numeros_acertados))
         f.write(
-            f"Sorteio: {sorteio} | Aposta: {aposta} | Acertos: {acertos} | Números Acertados: {texto_acertados}\n"
+            f" Id:{gerar_id} Sorteio: {sorteio} | Aposta: {aposta} | Acertos: {acertos} | Números Acertados: {texto_acertados}\n"
         )
