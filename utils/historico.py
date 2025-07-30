dbc9758e4cf7b48f871431766ba4945b23b5710c
Yuri Fabio <yuri.fabio@escolar.ifrn.edu.br>
2025-07-30 09:23:24 -0300

    Adcionando lista de tarefas

--- utils/historico.py
+++ utils/historico.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 def historico(sorteio, aposta, acertos, numeros_acertados):
+    data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     with open("dados/historico.txt", 'a', encoding="utf-8") as f:
-        f.write(f"Sorteio: {sorteio} | Aposta: {aposta} | Acertos: {acertos} | Números Acertados: {numeros_acertados}\n")
+        f.write(f"{data_hora} Sorteio: {sorteio} | Aposta: {aposta} | Acertos: {acertos} | Números Acertados: {numeros_acertados}\n")
 
