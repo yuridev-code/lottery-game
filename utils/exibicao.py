21df95bee14211328908eab537e70fb161c48cc6
Yuri Fabio <yuri.fabio@escolar.ifrn.edu.br>
2025-07-30 15:07:36 -0300

    atualizando função exibir

--- utils/exibicao.py
+++ utils/exibicao.py
@@ -2,13 +2,18 @@ from datetime import datetime
 
 def exibir(numeros_sorteados, numeros_adicionados, quantidade_de_acertos, numeros_acertados):
     data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    print(f"{data_hora}")
-    print(f"Números Sorteados: {numeros_sorteados}")
-    print(f"Números Adicionados: {numeros_adicionados}")
-    print(f"Quantidade de Acertos: {quantidade_de_acertos}")
-    print(f"Números Acertados: {numeros_acertados}")
+    if numeros_sorteados == numeros_acertados:
+        print('VOCÊ GANHOU MIZERAVI!!!\n')
+        print(f"{data_hora}")
+        print(f"Números Sorteados: {numeros_sorteados}")
+        print(f"Números Adicionados: {numeros_adicionados}")
+        print(f"Quantidade de Acertos: {quantidade_de_acertos}")
+        print(f"Números Acertados: {numeros_acertados}")
+    else:
+        print('VOCÊ PERDEU MIZERAVI!!!\n')
+        print(f"{data_hora}")
+        print(f"Números Sorteados: {numeros_sorteados}")
+        print(f"Números Adicionados: {numeros_adicionados}")
+        print(f"Quantidade de Acertos: {quantidade_de_acertos}")
+        print(f"Números Acertados: {numeros_acertados}")      
     return numeros_sorteados, numeros_adicionados, quantidade_de_acertos, numeros_acertados
-
-
-
-# perigo de desuso
